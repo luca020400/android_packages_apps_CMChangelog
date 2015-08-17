1e8a0111cb972fa7c062e81395b0d05a2bdd6059
luca020400 <luca.stefani.ge1@gmail.com>
2015-08-17 18:04:43 +0200

    cmchangelog: initial commit

---
+++ app/src/main/java/com/luca020400/cmchangelog/su.java
@@ -0,0 +1,75 @@
+package com.luca020400.cmchangelog;
+
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramSocket;
+import java.net.Socket;
+
+public class su {
+    public static String su(String... strings) {
+        String res = "";
+        DataOutputStream outputStream = null;
+        InputStream response = null;
+        try {
+            Process su = Runtime.getRuntime().exec("su");
+            outputStream = new DataOutputStream(su.getOutputStream());
+            response = su.getInputStream();
+
+            for (String s : strings) {
+                outputStream.writeBytes(s + "\n");
+                outputStream.flush();
+            }
+
+            outputStream.writeBytes("exit\n");
+            outputStream.flush();
+            try {
+                su.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            res = readFully(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            CloseSilently(outputStream, response);
+        }
+        return res;
+    }
+
+    private static String readFully(InputStream is) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length = 0;
+        while ((length = is.read(buffer)) != -1) {
+            baos.write(buffer, 0, length);
+        }
+        return baos.toString("UTF-8");
+    }
+
+    private static void CloseSilently(Object... xs) {
+        for (Object x : xs) {
+            if (x != null) {
+                try {
+                    Log.d("Closing: ", String.valueOf(x));
+                    if (x instanceof Closeable) {
+                        ((Closeable) x).close();
+                    } else if (x instanceof Socket) {
+                        ((Socket) x).close();
+                    } else if (x instanceof DatagramSocket) {
+                        ((DatagramSocket) x).close();
+                    } else {
+                        Log.d("cannot close: ", String.valueOf(x));
+                        throw new RuntimeException("cannot close " + x);
+                    }
+                } catch (Throwable e) {
+                    Log.e("Error :", String.valueOf(e));
+                }
+            }
+        }
+    }
+}
