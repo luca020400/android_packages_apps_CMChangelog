f672a198a271c96fdbd3527ede72c250d11efd8e
luca020400 <luca.stefani.ge1@gmail.com>
2015-08-17 23:12:50 +0200

    ASynka

--- app/src/main/java/com/luca020400/cmchangelog/MainActivity.java
+++ app/src/main/java/com/luca020400/cmchangelog/MainActivity.java
@@ -1,8 +1,8 @@
 package com.luca020400.cmchangelog;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,137 +13,36 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class MainActivity extends Activity {
     public String mDevice;
     public String mCyanogenMod;
+    public String mLastUpdates;
+    public String mRepo;
+    public String mId;
+    public String mSubject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDevice = cmd.exec("getprop ro.cm.device | awk '{print $1}'");
-        mCyanogenMod = cmd.exec("getprop ro.cm.version | cut -f1 -d'-' | awk '{print $1}'");
+        String[] version = cmd.exec("getprop ro.cm.version").split("-");
+        mDevice = version[3];
+        mCyanogenMod = version[0];
 
-        Log.i("Device", String.valueOf(mDevice));
-        Log.i("CyanogenMod", String.valueOf(mCyanogenMod));
+        Log.i("Device", "'" + String.valueOf(mDevice) + "'");
+        Log.i("CyanogenMod", "'" + String.valueOf(mCyanogenMod) + "'");
 
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                .detectAll()
-                .penaltyLog()
-                .penaltyDialog()
-                .build());
-
-        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll()
-                .penaltyLog()
-                .build());
-
-        // Assume it's known
-        final int ROW_ITEMS = 3;
-
-        final class GridAdapter extends BaseAdapter {
-
-            final ArrayList<String> mItems;
-            final int mCount;
-
-            /**
-             * Default constructor
-             * @param items to fill data to
-             */
-            private GridAdapter(final ArrayList<String> items) {
-
-                mCount = items.size() * ROW_ITEMS;
-                mItems = new ArrayList<String>(mCount);
-
-                // for small size of items it's ok to do it here, sync way
-                for (String item : items) {
-                    // get separate string parts, divided by ,
-                    final String[] parts = item.split(",");
-
-                    // remove spaces from parts
-                    for (String part : parts) {
-                        part.replace(" ", "");
-                        mItems.add(part);
-                    }
-                }
-            }
-
-            @Override
-            public int getCount() {
-                return mCount;
-            }
-
-            @Override
-            public Object getItem(final int position) {
-                return mItems.get(position);
-            }
-
-            @Override
-            public long getItemId(final int position) {
-                return position;
-            }
-
-            @Override
-            public View getView(final int position, final View convertView, final ViewGroup parent) {
-
-                View view = convertView;
-
-                if (view == null) {
-                    view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
-                }
-
-                final TextView text = (TextView) view.findViewById(android.R.id.text1);
-
-                text.setText(mItems.get(position));
-
-                return view;
-            }
-        }
-
-        final GridView grid = (GridView) findViewById(R.id.gridView);
-        final ArrayList<String> items = new ArrayList<String>();
-        grid.setAdapter(new GridAdapter(items));
-
-        JSONObject json = null;
-        String str = "";
-        HttpResponse response;
-        HttpClient myClient = new DefaultHttpClient();
-        HttpPost myConnection = new HttpPost(String.format("http://api.cmxlog.com/changes/%s/%s", mCyanogenMod.substring(0, 3), mDevice));
-
-        try {
-            response = myClient.execute(myConnection);
-            str = EntityUtils.toString(response.getEntity(), "UTF-8");
-
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            JSONArray jArray = new JSONArray(str);
-            json = jArray.getJSONObject(0);
-            json.getString("repo");
-            json.getString("last_updated");
-            json.getString("id");
-            json.getString("subject");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        new UpdateTask().execute(String.format
+                ("http://api.cmxlog.com/changes/%s/%s", mCyanogenMod, mDevice));
     }
 
     @Override
@@ -158,5 +57,99 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class GridAdapter extends BaseAdapter {
+
+        // Assume it's known
+        final int ROW_ITEMS = 2;
+
+        final ArrayList<String> mItems;
+        final int mCount;
+
+        /**
+         * Default constructor
+         *
+         * @param items to fill data to
+         */
+        private GridAdapter(final ArrayList<String> items) {
+
+            mCount = items.size() * ROW_ITEMS;
+            mItems = new ArrayList<>(mCount);
+
+            // for small size of items it's ok to do it here, sync way
+            for (String item : items) {
+                // get separate string parts, divided by ,
+                final String[] parts = item.split(",");
+
+                // remove spaces from parts
+                for (String part : parts) {
+                    mItems.add(part);
+                }
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return mCount;
+        }
+
+        @Override
+        public Object getItem(final int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public long getItemId(final int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, final View convertView, final ViewGroup parent) {
+
+            View view = convertView;
+
+            if (view == null) {
+                view = LayoutInflater.from(parent.getContext()).inflate
+                        (android.R.layout.simple_list_item_1, parent, false);
+            }
+
+            final TextView text = (TextView) view.findViewById(android.R.id.text1);
+
+            text.setText(mItems.get(position));
+
+            return view;
+        }
+    }
+
+    private class UpdateTask extends AsyncTask<String, String, String> {
+        protected String doInBackground(String... urls) {
+            JSONObject json;
+            try {
+                JSONArray jArray = new JSONArray(Jsoup.connect(urls[0]).ignoreContentType(true).get());
+                json = jArray.getJSONObject(0);
+                mRepo = json.getString("repo");
+                mLastUpdates = json.getString("last_updated");
+                mId = json.getString("id");
+                mSubject = json.getString("subject");
+            } catch (JSONException e) {
+                // json shitted itself
+                e.printStackTrace();
+            } catch (IOException e) {
+                // can't connect to url (jsoup shitted itself)
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute() {
+            final GridView grid = (GridView) findViewById(R.id.gridView);
+            final ArrayList<String> items = new ArrayList<>();
+
+            items.add(mRepo);
+            items.add(mSubject);
+
+            grid.setAdapter(new GridAdapter(items));
+        }
     }
 }
